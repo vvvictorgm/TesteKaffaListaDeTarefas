e7aa5ac08862c95e1e0e155302c1828d6e4703c9
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-14 13:41:38 -0300

    Criado nova Main e Intent

--- app/src/main/java/com/testeKaffa/tarefas/MainActivity.java
+++ app/src/main/java/com/testeKaffa/tarefas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.testeKaffa.tarefas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -22,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), AdicionarTarefa.class);
+                startActivity();
             }
         });
     }
