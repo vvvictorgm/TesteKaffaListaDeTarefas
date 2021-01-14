8eab31a003b5e617886a4915693586bdf0f35104
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-14 18:22:00 -0300

    Update DbHelper.java

--- app/src/main/java/com/testeKaffa/tarefas/bancoDeDados/DbHelper.java
+++ app/src/main/java/com/testeKaffa/tarefas/bancoDeDados/DbHelper.java
@@ -9,13 +9,15 @@ import androidx.annotation.Nullable;
 public class DbHelper extends SQLiteOpenHelper {
     public static int VERSION = 1;
     public static String NOME_DB = "DB_EXERCICIO_TAREFAS";
-
+    public static String TABELA_TAREFAS = "tarefas";
     public DbHelper(@Nullable Context context) {
         super(context, NOME_DB, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //create a SQL table at first use and create a ID with auto increment
+        String sql ="CREATE TABLE IF NOT EXISTS "+TABELA_TAREFAS+" (id INTEGER PRIMARY KEY AUTO INCREMENT, nome TEXT NOT NULL); ";
 
     }
 
