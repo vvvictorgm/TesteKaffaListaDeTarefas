cfc16cf0cf8f0731d2200f9372cf6a57eff888b0
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-14 19:15:00 -0300

    Update DAO.java

--- app/src/main/java/com/testeKaffa/tarefas/bancoDeDados/DAO.java
+++ app/src/main/java/com/testeKaffa/tarefas/bancoDeDados/DAO.java
@@ -1,5 +1,28 @@
 package com.testeKaffa.tarefas.bancoDeDados;
 
-public class DAO {
+import com.testeKaffa.tarefas.Model.Tarefa;
 
+import java.util.List;
+
+public class DAO implements InterfaceDAO{
+
+    @Override
+    public boolean salvar(Tarefa tarefa) {
+        return false;
+    }
+
+    @Override
+    public boolean atualizar(Tarefa tarefa) {
+        return false;
+    }
+
+    @Override
+    public boolean deletar(Tarefa tarefa) {
+        return false;
+    }
+
+    @Override
+    public List<Tarefa> listar() {
+        return null;
+    }
 }
